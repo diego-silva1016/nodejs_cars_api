2cb8e53b14642beba02e565c94821f70119ee0c0
Diego Ribeiro Alvarenga Silva <diego2222110@gmail.com>
2023-10-10 00:29:34 -0300

    Teste

--- src/database/config.js
+++ src/database/config.js
@@ -1,5 +1,5 @@
 const knexConfig =  require('../../knexfile')[process.env.NODE_ENV || 'development'];
-
+console.log(process.env.NODE_ENV)
 const knex = require('knex')(knexConfig);
 
 module.exports = knex;
